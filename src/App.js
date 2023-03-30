7bde830d7296b61f3f328a08fd153ab821b1d3af
CarpeDiem0110 <eEren_Ceylan@hotmail.com>
2023-03-31 00:07:37 +0300

    main folder is modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useState } from "react";
 function App() {
-  return (
+    const [item,setItem] = useState("");  
+      const [items,setItems] = useState([]);
+
+
+   function addItem(){
+    
+    /* items is a object and it have 2 properity. 
+       id is unique for each element in array and value is our string expression 
+       which comes from input */ 
+    const items = {
+      id: Math.floor(Math.random * 1000),
+      value: item,
+    }
+
+    setItems(oldItems => [...oldItems,items])
+    setItem("")
+  }
+
+  /* This variable have same function with document.createElement in Javascript */ 
+  const newItemList = items.map((item) => 
+      {return <li key={item.id}>{item.value}</li>})
+
+  
+  /* I have taken the text with onChange from inputField
+      Then, I used setItem to take the text */ 
+
+
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Todo List App</h3>
+
+    
+      <input
+        type="text"
+        placeholder='Add a item'
+      
+       onChange  = {e => setItem(e.target.value)}
+      />
+
+     <br></br>
+      <button onClick={() => addItem()}> Add </button>
+     
+      <ul>
+        <li>Evden gel</li>
+        <li>5 litre su iç</li>
+        <li>Düzenli şekilde koşu yap</li>
+       {newItemList}
+      </ul>
     </div>
   );
 }
